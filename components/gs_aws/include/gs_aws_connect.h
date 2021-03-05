9a216069cabbdd2075965d1544596e4c59a2336f
mattziggs <mrh470@mun.ca>
2021-03-05 18:33:08 -0330

    moved config info to new file

--- components/gs_aws/include/gs_aws_connect.h
+++ components/gs_aws/include/gs_aws_connect.h
@@ -1,3 +1,6 @@
-
+/*
+ * This file isn't very exciting, just wanted to make the task
+ * visible to main so that it can be pinned to the core.
+ */
 
 void aws_iot_task(void *param);
--- components/gs_aws/src/gs_aws_connect.c
+++ components/gs_aws/src/gs_aws_connect.c
@@ -19,97 +19,15 @@
 #include "aws_iot_version.h"
 #include "aws_iot_mqtt_client_interface.h"
 
-const char* DEVICE_CERT =
-"-----BEGIN CERTIFICATE-----\n"
-"MIIDWTCCAkGgAwIBAgIUQNvXMOYfYq9L6GY0t4cCQrEjbBswDQYJKoZIhvcNAQEL\n"
-"BQAwTTFLMEkGA1UECwxCQW1hem9uIFdlYiBTZXJ2aWNlcyBPPUFtYXpvbi5jb20g\n"
-"SW5jLiBMPVNlYXR0bGUgU1Q9V2FzaGluZ3RvbiBDPVVTMB4XDTIxMDMwNTE2Mjcy\n"
-"MFoXDTQ5MTIzMTIzNTk1OVowHjEcMBoGA1UEAwwTQVdTIElvVCBDZXJ0aWZpY2F0\n"
-"ZTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMPHrsGhKaQzUozcFgXS\n"
-"6iAQMgM6GOXnWsEwt88U/WUXPwYOQAxdLjlc8kQhBHVDRF4TNGRDRlTkQSTzvGOS\n"
-"kY52XyDab6ARM5AEp8GLPsSzvquKWdUH5jPD0L8SE2nIchQ+CmfvMmN17dDE790b\n"
-"j6+1iqhwFST7V/OBZV3tYlCvhuA/9TU+DDj9BHbg4y7plkpx6sn2OQEHq7QdCjN+\n"
-"K+nTSGuApg4/3GobZQTs1Xh4R1asvuEfpHiweqX8QoKwTzx4rXR6EN5X+RaQonw3\n"
-"g5ZYensGsMqZFqzNrTp8irdvwCW81xNlfhGpZh3y9zwzznLNKuKxGvOq05sXtcVK\n"
-"P4MCAwEAAaNgMF4wHwYDVR0jBBgwFoAU/hhdfxuPJcR+I7FThdheop28ERIwHQYD\n"
-"VR0OBBYEFIE4ec1M4iitQFNgfM5dbgbRvYswMAwGA1UdEwEB/wQCMAAwDgYDVR0P\n"
-"AQH/BAQDAgeAMA0GCSqGSIb3DQEBCwUAA4IBAQCqRkNQ/AsU+UYIgFeSwzyVM59e\n"
-"CDXD1tT7DpsgXOwLsNef35yDp8qBPKNotFNy+7Ck1tCtnNqqQ5bgfiaCxNNzKmg8\n"
-"N3jFVh80AATtqRLPu44UB5U9hdP5GgtjuhWX3SOcFB88mVj/gKSYTZDo3OPItNtX\n"
-"fenUol3Cv6whtq5m69HeFzwGZmT9EcATE6UZEiX1svRsmEVfXifCbmBs3kFCrco1\n"
-"XFTR47ilMnWFk+rES0TGCAZ4XXjV47xgHum4xBVnE6fmo4x+jYgTW62plXl9ls3d\n"
-"0nAL6fladAj1MIRjjno8jx5/vlDpviYjy/glwi0ATJRx0drialAOVDWHhY+Y\n"
-"-----END CERTIFICATE-----";
-
-const char* ROOT_CA =
-"-----BEGIN CERTIFICATE-----\n"
-"MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF\n"
-"ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6\n"
-"b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL\n"
-"MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv\n"
-"b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj\n"
-"ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM\n"
-"9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw\n"
-"IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6\n"
-"VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L\n"
-"93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm\n"
-"jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC\n"
-"AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA\n"
-"A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI\n"
-"U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs\n"
-"N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv\n"
-"o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU\n"
-"5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy\n"
-"rqXRfboQnoZsG4q5WTP468SQvvG5\n"
-"-----END CERTIFICATE-----";
-
-const char* PRIV_KEY =
-"-----BEGIN RSA PRIVATE KEY-----\n"
-"MIIEpAIBAAKCAQEAw8euwaEppDNSjNwWBdLqIBAyAzoY5edawTC3zxT9ZRc/Bg5A\n"
-"DF0uOVzyRCEEdUNEXhM0ZENGVORBJPO8Y5KRjnZfINpvoBEzkASnwYs+xLO+q4pZ\n"
-"1QfmM8PQvxITachyFD4KZ+8yY3Xt0MTv3RuPr7WKqHAVJPtX84FlXe1iUK+G4D/1\n"
-"NT4MOP0EduDjLumWSnHqyfY5AQertB0KM34r6dNIa4CmDj/cahtlBOzVeHhHVqy+\n"
-"4R+keLB6pfxCgrBPPHitdHoQ3lf5FpCifDeDllh6ewawypkWrM2tOnyKt2/AJbzX\n"
-"E2V+EalmHfL3PDPOcs0q4rEa86rTmxe1xUo/gwIDAQABAoIBADPamZCzbOIOrdid\n"
-"GkAkotonL/fm1V1fL4xnqOdyOcMiqxChiZk8vNd4pLoivJmKXXtFPuUnZGNrqU1u\n"
-"OQFueprvAzciY9pTdAK7GOuMQ+25ZdDtbemck/416P4SfY3RB1YRMJRiqEfrnjdj\n"
-"UdyQRjhVrwn51b/bymm2ZfDAxP91ZqUlcfW1wjghoxqJ256PNGLOtcFYWxiJbcVT\n"
-"ngdcQRBzPIDM4Zfd9ZTeaJWx61+i0Q6J0/xCOZMdQJ9C1ZTIcvBa5U1PAPy7rhKl\n"
-"XeI4oK5bZ2MnGiAYYrvsq2e2iIIEUEq5ywoIPIJyTt/EViYqMDhMdiRCgaunxcGY\n"
-"TE9ymvECgYEA/E4h3V8ygQ8l4kHJS/Rs3NT1UsOELw43zu/ghtMYWd0wibngHxdS\n"
-"Pg94H9Cs3NqZfFeLmFvzoonnbEVR37/YXlIkwda3QzxIByiZw/WWMfjsLgA9j8oY\n"
-"+wZuxFm9bKPg1upx/GWGcZl3uGrjkdtawOPgfVnunXHxmzZcEXWeEV0CgYEAxqWk\n"
-"lK191/HFCLhHQTXnMVyp+wtH//crccWLEmP9/JKpz1vcQHT5KOqUrXj/rrwK2bmQ\n"
-"UKPQzo9DeHB70/HcR/oj70VTcxy6c8w6hsAce5p43jarymHzA5sNiexPYayaeBjl\n"
-"80CY9QJ9s3M9y5OEkcEeE/cCDmQeBLmX89ZmJl8CgYB7CUL8aAsB4rgvsLMQJh7c\n"
-"RcFoXc3dAIr4kAOm29Q5JmMgFpOTtdS2TObgZOuXO8azWyHI57ZUoflH1DoL6csC\n"
-"m/4uxJ1T3Vamnwb1B5L5xZCvEhDzSLOL3mQvvi9ghj4DsApr8bpxNUyrkvxsyVJl\n"
-"U/rKRgvVnZYbgZO+M+QkRQKBgQCJPZuNVKei79gvBBHGjdUil2CgntSplTygp9hD\n"
-"Zm3td+514oMyzR3vFNKPXw9Wy3e7oKaB8AutCfGVXjFO5sRHvhvhjUXwO6LdqfWt\n"
-"b0C+bKSg8aiSdEWO8irSZT8avkMiQiFsg+6pJ4Vs22uQIGTwxjlflcIH60eKACdp\n"
-"QXDlhQKBgQCSxj1iqL198E0jS+N7I6t4xPegKbidFtB25mIXtcMvP9M+lIS7oEgA\n"
-"OroQ6/vlsWTUW4Djs//8222HfAuqLq9Y/4Q0EJegnxpHrbKSJVJGH4gnAU/BQ8ZE\n"
-"mqkFpsbzPkXqGtIlEkJPmg3k34B5UEhfe+VMBt/gzZA5yDLFhXYOqQ==\n"
-"-----END RSA PRIVATE KEY-----";
+#include "gs_aws_config.h"
 
 const char* TOPIC = "topics/BankInfoStatus";
 const int CONNECTED_BIT = BIT0;
 static const char *TAG = "gs_aws";
 const char* CLIENT_ID = "Main_Pedal";
 
-/*
- * @brief Default MQTT HOST URL is pulled from the aws_iot_config.h
- */
-char HostAddress[255] = AWS_IOT_MQTT_HOST;
-
-/**
-* @brief Default MQTT port is pulled from the aws_iot_config.h
-*/
-uint32_t port = AWS_IOT_MQTT_PORT;
-
 void iot_subscribe_callback_handler(AWS_IoT_Client *pClient, char *topicName, uint16_t topicNameLen,
                                     IoT_Publish_Message_Params *params, void *pData) {
-    //ESP_LOGI(TAG, "Subscribe callback");
     ESP_LOGI(TAG, "%.*s\t%.*s", topicNameLen, topicName, (int) params->payloadLen, (char *)params->payload);
 }
 
@@ -145,13 +63,10 @@ void aws_iot_task(void *param) {
     IoT_Client_Init_Params mqttInitParams = iotClientInitParamsDefault;
     IoT_Client_Connect_Params connectParams = iotClientConnectParamsDefault;
 
-    IoT_Publish_Message_Params paramsQOS0;
-    IoT_Publish_Message_Params paramsQOS1;
-
     ESP_LOGI(TAG, "AWS IoT SDK Version %d.%d.%d-%s", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, VERSION_TAG);
 
     mqttInitParams.enableAutoReconnect = false; // We enable this later below
-    mqttInitParams.pHostURL = "a1lyjy5lwlbc46-ats.iot.us-east-1.amazonaws.com";
+    mqttInitParams.pHostURL = HOST_URL;
     mqttInitParams.port = 443;
     mqttInitParams.pRootCALocation = ROOT_CA;
     mqttInitParams.pDeviceCertLocation = DEVICE_CERT;
@@ -207,14 +122,6 @@ void aws_iot_task(void *param) {
 
     sprintf(cPayload, "%s : %d ", "hello from SDK", i);
 
-    paramsQOS0.qos = QOS0;
-    paramsQOS0.payload = (void *) cPayload;
-    paramsQOS0.isRetained = 0;
-
-    paramsQOS1.qos = QOS1;
-    paramsQOS1.payload = (void *) cPayload;
-    paramsQOS1.isRetained = 0;
-
     while((NETWORK_ATTEMPTING_RECONNECT == rc || NETWORK_RECONNECTED == rc || SUCCESS == rc)) {
 
         //Max time the yield function will wait for read messages
@@ -224,19 +131,6 @@ void aws_iot_task(void *param) {
             continue;
         }
 
-//        //ESP_LOGI(TAG, "Stack remaining for task '%s' is %d bytes", pcTaskGetTaskName(NULL), uxTaskGetStackHighWaterMark(NULL));
-//        vTaskDelay(1000 / portTICK_RATE_MS);
-//        sprintf(cPayload, "%s : %d ", "hello from ESP32 (QOS0)", i++);
-//        paramsQOS0.payloadLen = strlen(cPayload);
-//        rc = aws_iot_mqtt_publish(&client, TOPIC, TOPIC_LEN, &paramsQOS0);
-//
-//        //sprintf(cPayload, "%s : %d ", "hello from ESP32 (QOS1)", i++);
-//        paramsQOS1.payloadLen = strlen(cPayload);
-//        rc = aws_iot_mqtt_publish(&client, TOPIC, TOPIC_LEN, &paramsQOS1);
-//        if (rc == MQTT_REQUEST_TIMEOUT_ERROR) {
-//            ESP_LOGW(TAG, "QOS1 publish ack not received.");
-//            rc = SUCCESS;
-//        }
     }
 
     ESP_LOGE(TAG, "An error occurred in the main loop.");
