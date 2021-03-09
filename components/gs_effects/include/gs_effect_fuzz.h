2ee84b74fa062e1cd287d98e46d6dfdf9ae33b50
bfh531 <bfh531@mun.ca>
2021-03-08 22:04:21 -0330

    took out trem_init from gs_effect_fuzz; not needed.

--- components/gs_effects/include/gs_effect_fuzz.h
+++ components/gs_effects/include/gs_effect_fuzz.h
@@ -4,8 +4,6 @@
 
 void set_fuzz_state(bool state);
 
-void trem_init();
-
 void gs_fuzz_effect(int16_t *input_buffer, size_t input_bytes_read, int16_t *output_buffer);
 
 void set_fuzz_gain(float gain);
